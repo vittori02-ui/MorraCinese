7b58bc3107079c4f2241a6791bc900aca7176983
vittori02-ui <vittorioprivitera02@gmail.com>
2026-04-29 23:30:54 +0200

    edit 1

---
+++ MorraCineseMVC/src/mainMorraCinese/MainClass.java
@@ -0,0 +1,16 @@
+package mainMorraCinese;
+import vcMorraCinese.GameCineseVC;
+/**
+ *
+ * @author Vittorio Privitera
+ */
+public class MainClass {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameCineseVC gc=new GameCineseVC();
+        gc.setVisible(true);
+        gc.setLocationRelativeTo(null);
+    } 
+}
